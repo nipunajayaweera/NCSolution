e633a2eca0570c6ee3156533e7cd6f90d53cad9a
Chathuranga.Sandun <Chathurangasandunkumara@gmail.com>
2017-07-17 23:52:53 +0530

    major change to database, adding 3 tables

---
+++ NCSolution.DomainModel/Model/SavedPaper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCSolution.DomainModel.Model
+{
+    public class SavedPaper
+    {
+        [Key ,Column(Order = 0)]
+        public int SavedPaperId { get; set; }
+
+        
+        [ForeignKey("Exam")]
+        public int ExamId { get; set; }
+        public Exam Exam { get; set; }
+
+        
+        [ForeignKey("Question")]
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
+}
